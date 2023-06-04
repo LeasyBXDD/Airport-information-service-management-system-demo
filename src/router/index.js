bf06d6790256556f81a93b2ae1d74e79bc46c98d
Leeasy <793556890@qq.com>
2023-06-04 13:13:18 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/choose',
+      name: 'choose',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ChooseView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/ChooseView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Choose</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChooseView",
+};
+</script>
+
+<style scoped>
+
+</style>
